4d1106556262db0aa7c2a08d5dfabe7c0994a831
elzup <guild0105@gmail.com>
2017-02-16 01:55:58 +0900

    Install Lint, flows

---
+++ App/Containers/App.js
@@ -0,0 +1,21 @@
+/* @flow */
+
+import React, { Component } from 'react';
+import RootContainer from './RootContainer';
+
+const a: string = 10
+
+class App extends Component {
+  render() {
+    const text = 1;
+    return (
+      <View>
+        <Text>
+          Hello!
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default App;
