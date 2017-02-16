69a531c527b9f754d8f43cf5ad07d138e01228da
elzup <guild0105@gmail.com>
2017-02-16 13:14:31 +0900

    Create list

--- App/Containers/HomeScreen.js
+++ App/Containers/HomeScreen.js
@@ -4,24 +4,56 @@ import React, { Component } from 'react';
 import {
   View,
   Text,
+  ListView,
 } from 'react-native';
 import feedClient from '../Services/FeedClient';
 import type { Feed } from '../Services/FeedClient';
+import { List, ListItem } from 'react-native-elements'
 
 class HomeScreen extends Component {
+
+  state: {
+    dataSource: Object
+  }
+
+  constructor(props: Object) {
+    super(props);
+    const rowHasChanged = (r1: Feed, r2: Feed) => r1.title + r1.blogname !== r2.title + r2.blogname
+    const ds = new ListView.DataSource({rowHasChanged})
+    this.state = {
+      dataSource: ds.cloneWithRows([])
+    }
+  }
+
   componentDidMount() {
     feedClient.fetchFeed().then((feeds: Array<Feed>) => {
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(feeds)
+      });
     }).done();
   }
 
-  render() {
+  renderRow (feed: Feed, sectionID: number) {
     return (
-      <View>
-        <Text>
-          Hello
-        </Text>
-      </View>
-    );
+      <ListItem
+        roundAvatar
+        key={sectionID}
+        title={feed.title}
+        subtitle={feed.blogname}
+      />
+    )
   }
+
+  render () {
+    return (
+      <List>
+        <ListView
+          renderRow={this.renderRow}
+          dataSource={this.state.dataSource}
+        />
+      </List>
+    )
+  }
+
 }
 export default HomeScreen;
