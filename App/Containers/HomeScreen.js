70e963b7e729517eb82f96e0c0321637fec00b69
elzup <guild0105@gmail.com>
2017-02-16 11:40:08 +0900

    Flow :fire:

--- App/Containers/HomeScreen.js
+++ App/Containers/HomeScreen.js
@@ -3,12 +3,18 @@
 import React, { Component } from 'react';
 import {
   View,
-  Text
-} from 'react-native'
+  Text,
+} from 'react-native';
+import feedClient from '../Services/FeedClient';
+import type { Feed } from '../Services/FeedClient';
 
 class HomeScreen extends Component {
+  componentDidMount() {
+    feedClient.fetchFeed().then((feeds: Array<Feed>) => {
+    }).done();
+  }
+
   render() {
-    const text: string = 1;
     return (
       <View>
         <Text>
