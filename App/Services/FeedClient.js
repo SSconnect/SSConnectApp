a674c33301d8f0bc99b44f217b10bfa29ef318a3
elzup <guild0105@gmail.com>
2017-02-26 00:43:29 +0900

    Fix feedlist async

--- App/Services/FeedClient.js
+++ App/Services/FeedClient.js
@@ -14,28 +14,23 @@ class FeedClient {
 	constructor() {
 	}
 
-	fetchFeed() {
+	async fetchFeed() {
 		const parser = new DOMParser();
-		return fetch('http://ssmatomesokuho.com/feed.xml')
-    .then(res => res.text())
-    .then(text => parser.parseFromString(text, 'application/xml'))
-    .then(doc => doc.documentElement.getElementsByTagName('item'))
-    .then(items => {
-	const feeds: Array<Feed> = [];
-	for (let i = 0; i < items.length; i++) {
-		const item = items[i];
-		feeds.push({
-			blogname: this.elementValue(item, 'dc:creator'),
-			title: this.elementValue(item, 'title'),
-			link: this.elementValue(item, 'link'),
-			read: false,
-			description: this.elementValue(item, 'description')
+		const res = await fetch('http://ssmatomesokuho.com/feed.xml');
+		const doc = parser.parseFromString(res.text(), 'application/xml');
+		const items = doc.documentElement.getElementsByTagName('item');
+		const feeds = items.map(item => {
+			return {
+				blogname: this.elementValue(item, 'dc:creator'),
+				title: this.elementValue(item, 'title'),
+				link: this.elementValue(item, 'link'),
+				read: false,
+				description: this.elementValue(item, 'description')
+			};
 		});
+		return feeds;
 	}
-	return feeds;
-},
-    );
-	}
+
 	elementValue(e: Object, tagName: string) {
 		return e.getElementsByTagName(tagName)[0].childNodes[0].data;
 	}
