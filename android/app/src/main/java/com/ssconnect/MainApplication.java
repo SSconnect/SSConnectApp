11767ad465ed131f8c9ffce7067992299c85519e
elzup <guild0105@gmail.com>
2017-03-26 04:07:08 +0900

    RN upgrade

--- android/app/src/main/java/com/ssconnect/MainApplication.java
+++ android/app/src/main/java/com/ssconnect/MainApplication.java
@@ -1,13 +1,8 @@
 package com.ssconnect;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
-import com.react.rnspinkit.RNSpinkitPackage;
-import io.realm.react.RealmReactPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -27,10 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new RNSpinkitPackage(),
-            new RealmReactPackage(),
-            new VectorIconsPackage()
+          new MainReactPackage()
       );
     }
   };
