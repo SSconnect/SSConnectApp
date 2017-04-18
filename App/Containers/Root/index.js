4eba123eac1ffc78c02f3338135c3f48a5fb73fb
elzup <guild0105@gmail.com>
2017-04-18 21:00:18 +0900

    To redux

---
+++ App/Containers/Root/index.js
@@ -0,0 +1,57 @@
+// @flow
+
+import React, { Component } from 'react';
+import { View, StatusBar, StyleSheet } from 'react-native';
+import { connect } from 'react-redux';
+
+import StartupActions from '../Redux/StartupRedux';
+import ReduxPersist from '../Config/ReduxPersist';
+import NavigationRouter from '../../Navigation/NavigationRouter';
+
+// Styles
+const styles = StyleSheet.create({
+	applicationView: {
+		flex: 1,
+	},
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		backgroundColor: Colors.background,
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		fontFamily: Fonts.type.base,
+		margin: Metrics.baseMargin,
+	},
+	myImage: {
+		width: 200,
+		height: 200,
+		alignSelf: 'center',
+	},
+});
+
+class RootContainer extends Component {
+	componentDidMount() {
+		// if redux persist is not active fire startup action
+		if (!ReduxPersist.active) {
+			this.props.startup();
+		}
+	}
+
+	render() {
+		return (
+			<View style={styles.applicationView}>
+				<StatusBar barStyle="light-content" />
+				<NavigationRouter />
+			</View>
+		);
+	}
+}
+
+// wraps dispatch to create nicer functions to call within our component
+const mapDispatchToProps = dispatch => ({
+	startup: () => dispatch(StartupActions.startup()),
+});
+
+export default connect(null, mapDispatchToProps)(RootContainer);
