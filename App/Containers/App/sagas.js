0892a9c2edce5c9896a7c6b10cb31b70d4e34a77
elzup <guild0105@gmail.com>
2017-04-19 15:00:45 +0900

    From realm

--- App/Containers/App/sagas.js
+++ App/Containers/App/sagas.js
@@ -2,10 +2,12 @@ import { fork, put, takeLatest } from 'redux-saga/effects';
 
 import { LOAD_PROFILES } from './constants';
 import { loadProfilesEnd } from './actions';
+import { makeSelectTabProfiles } from './selectors';
 import type { TabProfile } from '../../../Types';
+import realm from '../../Models/RealmModel';
 
 export function* getTabProfiles() {
-	const profiles: array<TabProfile> = [{ value: 'P', type: 'search' }];
+	const profiles: array<TabProfile> = yield realm.getTabProfiles();
 	yield put(loadProfilesEnd(profiles));
 }
 
--- App/Store/configureStore.js
+++ App/Store/configureStore.js
@@ -1,10 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import createSagaMiddleware from 'redux-saga';
+import { fromJS } from 'immutable';
 import { createLogger } from 'redux-logger';
+
 import reducer from '../Containers/App/reducers';
 
 const loggerMiddleware = createLogger({
-	stateTransformer: state => state,
+	stateTransformer: state => fromJS(state).toJS(),
 });
 
 const sagaMiddleware = createSagaMiddleware();
