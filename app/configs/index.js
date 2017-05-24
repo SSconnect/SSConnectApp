8a5fd4a5aee4fd9d443a219a999a009beae04511
elzup <guild0105@gmail.com>
2017-05-24 16:41:11 +0900

    Moment ja

--- app/configs/index.js
+++ app/configs/index.js
@@ -1,4 +1,5 @@
 // @flow
+import moment from 'moment';
 
 const config = {
 	productID: 'com.elzup.ssconnect',
@@ -9,5 +10,21 @@ const config = {
 		},
 	},
 };
+
+moment.locale('ja', {
+	weekdays: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日'],
+	weekdaysShort: ['日', '月', '火', '水', '木', '金', '土'],
+	relativeTime: {
+		future: '%s',
+		past: '%s前',
+		s: '%d秒',
+		m: '1分',
+		mm: '%d分',
+		h: '1時間',
+		hh: '%d時間',
+		d: '1日',
+		dd: '%d日',
+	},
+});
 
 export default config;
