dc32bd9c8213f8fdd3a51af8a361f8b570280204
elzup <guild0105@gmail.com>
2017-05-26 10:52:01 +0900

    Kill add

--- app/containers/Web/index.js
+++ app/containers/Web/index.js
@@ -1,12 +1,12 @@
 // @flow
 
-import React from 'react'
-import { View, StyleSheet, WebView, Linking, Platform } from 'react-native'
-import { connect } from 'react-redux'
-import { Button } from 'react-native-elements'
-import { Actions } from 'react-native-router-flux'
+import React from "react"
+import { View, StyleSheet, WebView, Linking, Platform } from "react-native"
+import { connect } from "react-redux"
+import { Button } from "react-native-elements"
+import { Actions } from "react-native-router-flux"
 
-import { Scales } from '../../themes'
+import { Scales } from "../../themes"
 
 // Styles
 const styles = StyleSheet.create({
@@ -17,18 +17,18 @@ const styles = StyleSheet.create({
 })
 
 type Props = {
-	uri: string
-}
+  uri: string
+};
 
 type State = {
-	height: number
-}
+  height: number
+};
 
 class WebScreen extends React.Component {
-	props: Props
+	props: Props;
 	state: State = {
 		height: 40,
-	}
+	};
 
 	componentDidMount() {}
 
@@ -41,42 +41,45 @@ class WebScreen extends React.Component {
 		const { height } = this.state
 		return (
 			<View style={styles.applicationView}>
-				<WebView source={{ uri }} style={{ marginBottom: 40 }} />
+				<WebView source={{ uri }} style={{ marginBottom: -100 }} />
 				<View
 					style={{
-						flexDirection: 'row',
-						backgroundColor: '#ddd',
-						position: 'absolute',
+						flexDirection: "row",
+						backgroundColor: "#ddd",
+						position: "absolute",
 						bottom: 0,
 						left: 0,
 						right: 0,
 						height,
 					}}
-				>
+        >
 					<Button
 						style={{ flex: 1 }}
 						backgroundColor="#ddd"
 						onPress={() => {
 							this.setState({ height: height + 10 })
 						}}
-					/>
+          />
 					<Button
 						style={{ flex: 1 }}
 						backgroundColor="#ddd"
 						onPress={() => {
 							this.setState({ height: Math.max(height - 10, 40) })
 						}}
-					/>
+          />
 					<View style={{ flex: 2 }} />
 					<Button
 						style={{ flex: 2 }}
 						title="開く"
-						icon={{ type: 'font-awesome', name: Platform.OS === 'ios' ? 'safari' : 'chrome' }}
+						icon={{
+							type: "font-awesome",
+							name: Platform.OS === "ios" ? "safari" : "chrome",
+						}}
 						onPress={() => {
 							Actions.pop()
 							Linking.openURL(uri)
 						}}
-					/>
+          />
 				</View>
 			</View>
 		)
