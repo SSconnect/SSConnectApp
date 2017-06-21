e2b01e4b935bc71687f158497a5880c155ecafa5
elzup <guild0105@gmail.com>
2017-06-21 16:51:36 +0900

    Update Paginator layout2

--- app/components/Paginator/index.js
+++ app/components/Paginator/index.js
@@ -2,7 +2,16 @@
 
 import React from "react"
 import { Slider } from "react-native-elements"
-import { Row, Left, Right, Body, Button, Icon, Text } from "native-base"
+import {
+	Content,
+	Row,
+	Left,
+	Right,
+	Body,
+	Button,
+	Icon,
+	Text,
+} from "native-base"
 
 import type { PageInfo } from "../../types"
 
@@ -26,45 +35,47 @@ class Paginator extends React.PureComponent {
 	render() {
 		const { pageInfo, onComplete, onPressNext, onPressPrev } = this.props
 		return (
-			<Row>
-				<Left>
-					<Button iconLeft light onPress={onPressNext}>
-						<Icon name="arrow-back" />
-						<Text>Back</Text>
-					</Button>
-				</Left>
-				<Body>
-					<Text
-						style={{
-							flex: 1,
-							textAlign: "center",
-							paddingTop: 12,
-						}}
-					>
-						{this.state.previewPage}/{pageInfo.total}
-					</Text>
-					<Row>
-						<Slider
-							value={pageInfo.page}
-							style={{ flex: 1 }}
-							step={1}
-							thumbTintColor="#333"
-							maximumValue={pageInfo.total}
-							minimumValue={1}
-							onSlidingComplete={onComplete}
-							onValueChange={value => {
-								this.setState({ previewPage: value })
+			<Content style={{ margin: 10 }}>
+				<Row>
+					<Left>
+						<Button iconLeft light onPress={onPressNext}>
+							<Icon name="arrow-back" />
+							<Text>Back</Text>
+						</Button>
+					</Left>
+					<Body>
+						<Text
+							style={{
+								flex: 1,
+								textAlign: "center",
+								paddingTop: 12,
 							}}
-						/>
-					</Row>
-				</Body>
-				<Right>
-					<Button iconRight light onPress={onPressNext}>
-						<Text>Next</Text>
-						<Icon name="arrow-forward" />
-					</Button>
-				</Right>
-			</Row>
+						>
+							{this.state.previewPage}/{pageInfo.total}
+						</Text>
+					</Body>
+					<Right>
+						<Button iconRight light onPress={onPressNext}>
+							<Text>Next</Text>
+							<Icon name="arrow-forward" />
+						</Button>
+					</Right>
+				</Row>
+				<Row>
+					<Slider
+						value={pageInfo.page}
+						style={{ flex: 1 }}
+						step={1}
+						thumbTintColor="#333"
+						maximumValue={pageInfo.total}
+						minimumValue={1}
+						onSlidingComplete={onComplete}
+						onValueChange={value => {
+							this.setState({ previewPage: value })
+						}}
+					/>
+				</Row>
+			</Content>
 		)
 	}
 }
