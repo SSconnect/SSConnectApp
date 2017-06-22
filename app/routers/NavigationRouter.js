90ce0c3244fcb8a8af75640d35a1cf94e18bbe2c
elzup <guild0105@gmail.com>
2017-06-22 10:38:33 +0900

    Pre navigation bar

--- app/routers/NavigationRouter.js
+++ app/routers/NavigationRouter.js
@@ -1,30 +1,21 @@
 // @flow
 
-import React from 'react'
-import { StyleSheet } from 'react-native'
-import { Scene, Router } from 'react-native-router-flux'
-import _ from 'lodash'
+import React from "react"
+import { StyleSheet } from "react-native"
+import { Scene, Router } from "react-native-router-flux"
+import _ from "lodash"
 
-import BaseScreen from '../containers/Base'
-import MainScreen from '../containers/Main'
-import WebScreen from '../containers/Web'
-import TagScreen from '../containers/Tag'
-import NavigationDrawer from '../containers/NavigationDrawer'
+import BaseScreen from "../containers/Base"
+import MainScreen from "../containers/Main"
+import WebScreen from "../containers/Web"
+import TagScreen from "../containers/Tag"
+import NavigationDrawer from "../containers/NavigationDrawer"
 
-import { Colors, IconName } from '../themes'
+import { Colors, IconName } from "../themes"
 
 const Styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: Colors.white,
-	},
-	main: {
-		marginTop: 300,
-	},
-	navBar: {
-		backgroundColor: Colors.white,
-	},
-	tabBarStyle: {
 		backgroundColor: Colors.white,
 	},
 })
@@ -55,8 +46,18 @@ class NavigationRouter extends React.PureComponent {
 							iconName={IconName.tag}
 							{...tabAttrs}
 						/>
-						<Scene key="baseScreen" component={BaseScreen} title="結果" titleStyle={Styles.title} />
-						<Scene key="webScreen" component={WebScreen} {...tabAttrs} panHandlers={null} />
+						<Scene
+							key="baseScreen"
+							component={BaseScreen}
+							title="結果"
+							titleStyle={Styles.title}
+						/>
+						<Scene
+							key="webScreen"
+							component={WebScreen}
+							{...tabAttrs}
+							panHandlers={null}
+						/>
 					</Scene>
 				</Scene>
 			</Router>
